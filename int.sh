e66a29f5746cb1c95ff4f31e31fa3f6ba5f479f2
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2022-08-14 12:54:15 +0800

    Update int.sh

--- int.sh
+++ int.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+rand=$(tr </dev/urandom -dc a-zA-Z0-9 | head -c8)
 read -p "Domain : " domain
 read -p "Uuid   : " uuid
 read -p "path   : " path
 
-echo "vless://${uuid}@who.int:443?path=wss://who.int${path}&security=tls&encryption=none&type=ws&host=${domain}&sni=who.int#macis"
+echo "vless://${uuid}@who.int:443?path=wss://who.int${path}&security=tls&encryption=none&type=ws&host=${domain}&sni=who.int#${rand}"
