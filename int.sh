c619e4fc11a15d0fefebdee0691072f369fa5a05
SYFQSAM <89333348+syfqsamvpn@users.noreply.github.com>
2022-08-14 13:24:07 +0800

    Update int.sh

--- int.sh
+++ int.sh
@@ -1,7 +1,58 @@
 #!/bin/bash
-rand=$(tr </dev/urandom -dc a-zA-Z0-9 | head -c8)
-read -p "Domain : " domain
-read -p "Uuid   : " uuid
-read -p "path   : " path
+vmess_req() {
+    user=$(tr </dev/urandom -dc a-zA-Z0-9 | head -c8)
+    read -p "Domain : " domain
+    read -p "Uuid   : " uuid
+    read -p "path   : " path
 
-echo "vless://${uuid}@who.int:443?path=wss://who.int${path}&security=tls&encryption=none&type=ws&host=${domain}&sni=who.int#${rand}"
+    cat >/root/$user-tls.json <<EOF
+      {
+      "v": "0",
+      "ps": "${user}",
+      "add": "who.int",
+      "port": "443",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "${path}",
+      "type": "none",
+      "host": "${domain}",
+      "sni": "who.int",
+      "tls": "tls"
+}
+EOF
+
+    vmesslink1="vmess://$(base64 -w 0 /root/$user-tls.json)"
+    echo ""
+    echo "Config : $vmesslink1"
+}
+
+vless_req() {
+    user=$(tr </dev/urandom -dc a-zA-Z0-9 | head -c8)
+    read -p "Domain : " domain
+    read -p "Uuid   : " uuid
+    read -p "path   : " path
+
+    echo ""
+    echo "Config : vless://${uuid}@who.int:443?path=wss://who.int${path}&security=tls&encryption=none&type=ws&host=${domain}&sni=who.int#${user}"
+}
+
+protocol_req() {
+    echo -e "[1] Vmess"
+    echo -e "[2] Vless"
+    echo -ne "Protocol ? : "
+    read proto
+    case "$proto" in
+    1)
+        vmess_req
+        ;;
+    2)
+        vless_req
+        ;;
+    *)
+        protocol_req
+        ;;
+    esac
+}
+
+protocol_req
